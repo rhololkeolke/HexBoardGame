04c7b9148d01d0aade1aaa1715c5bfa737ead611
Devin Schwab <digidevin@gmail.com>
2013-05-17 17:36:43 +0800

    Added a jUnit class to test the MainMenu activity
    Currently have the framework testing if each button
    launches the appropriate activity.

---
+++ test/com/devinschwab/MainMenuActivityTest.java
@@ -0,0 +1,28 @@
+package com.devinschwab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainMenuActivityTest {
+	
+	@Test
+	public void playButtonLaunchesGameActivity() throws Exception {
+		fail("Implement me!");
+	}
+	
+	@Test
+	public void highscoresButtonLaunchesHighscoresActivity() throws Exception {
+		fail("Implement me!");
+	}
+	
+	@Test
+	public void aboutButtonLaunchesAboutActivity() throws Exception {
+		fail("Implement me!");
+	}
+}
