2ec0b500c6307c82a23766c73e43a6ed85d5e9f2
Devin Schwab <digidevin@gmail.com>
2013-05-17 17:24:51 +0800

    Made all of the buttons on the mainmenu the same size. Also made the MainMenu activity the buttons onClick event handler.

--- src/com/devinschwab/hexboardgame/MainMenu.java
+++ src/com/devinschwab/hexboardgame/MainMenu.java
@@ -1,15 +1,39 @@
 package com.devinschwab.hexboardgame;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainMenu extends Activity {
+/**
+ * 
+ * @author Devin
+ * 
+ * @brief Default Activity. Displays the game's main menu
+ *
+ */
+public class MainMenu extends Activity implements OnClickListener {
 
+	/**
+	 * @brief Creates the MainMenu activity
+	 * 
+	 * Sets up the click listener for the buttons.
+	 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);        
         setContentView(R.layout.activity_main_menu);
+        
+        Button playButton = (Button) findViewById(R.id.play_btn);
+        Button highscoresButton = (Button) findViewById(R.id.highscores_btn);
+        Button aboutButton = (Button) findViewById(R.id.about_btn);
+        
+        playButton.setOnClickListener(this);
+        highscoresButton.setOnClickListener(this);
+        aboutButton.setOnClickListener(this);
     }
 
 
@@ -19,5 +43,35 @@ public class MainMenu extends Activity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
+
+    /**
+     * @brief Provides the OnClickListener Callback function
+     * 
+     * checks which button was clicked and responds appropriately.
+     */
+	@Override
+	public void onClick(View v) {
+		switch(v.getId())
+		{
+		case R.id.play_btn:
+			//! @todo play_btn case: Implement Me!
+			Log.d("buton click", "MainMenu: play_btn was clicked");
+			break;
+		case R.id.highscores_btn:
+			//! @todo highscores_btn case: Implement me!
+			Log.d("button click", "MainMenu: highscores_btn was clicked");
+			break;
+		case R.id.about_btn:
+			//! @todo about_btn case: Implement me!
+			Log.d("button click", "MainMenu: about_btn was clicked");
+			break;
+		default:
+			Log.e("button click", "MainMenu: Unknown button was clicked: " + Integer.toString(v.getId()));
+			break;
+		}
+	}
+    
+    
     
 }
