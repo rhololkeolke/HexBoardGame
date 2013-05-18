2088f8106d97ac7f664d9a4de3bbe39acb72f5c4
Devin Schwab <digidevin@gmail.com>
2013-05-18 13:35:32 +0800

    Added a basic hextile class.

---
+++ src/com/devinschwab/hexboardgame/HexTile.java
@@ -0,0 +1,42 @@
+package com.devinschwab.hexboardgame;
+
+import android.graphics.Bitmap;
+
+public class HexTile {
+	
+	private Bitmap bitmap; // the actual bitmap
+	private int x; // the X coordinate
+	private int y; // the Y coordinate
+	
+	public HexTile(Bitmap bitmap, int x, int y) {
+		this.bitmap = bitmap;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+	
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = bitmap;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
